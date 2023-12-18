d1d1bd8122821dfd90ab1a273ca71d27e4bbd4b4
fathima-1234 <fathimasuhra19@gmail.com>
2023-12-18 13:27:49 +0530

    add catogory

--- src/components/user/Servicercard1.jsx
+++ src/components/user/Servicercard1.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import instance,  { BASE_URL } from '../../utils/axios';
-
+import './loader.css'
 function Servicercard1() {
   const [categoryData, setCategoryData] = useState([]);
  
  
- 
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,7 +17,7 @@ function Servicercard1() {
        
 
         setCategoryData(categories.data);
-       
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -43,7 +43,7 @@ function Servicercard1() {
      {/* <h1 className='text-center py-12 text-xl font-serif'> Our Events</h1> */}
      
      <h1 className='text-3xl mb-[70px] text-center py-12 '>What We Offer</h1>
-
+     {Array.isArray(categoryData) && categoryData.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
             { categoryData.map((category) => (
               <div
@@ -67,10 +67,12 @@ function Servicercard1() {
               </div>
             ))}
           </div>
-          {categoryData.map((category) => (
-        console.log('category.image:', category.image)
-      ))}
-
+      //     {categoryData.map((category) => (
+      //   console.log('category.image:', category.image)
+      // ))}
+      ) : (
+        <p className="text-center font-serif text-customColorA ">Loading...</p>
+      )}
    
       <div className='text-center mb-10'>
         <Link to='/home-list-event' className="p-2 overflow-hidden rounded-md bg-customColorA hover:bg-customColorD text-sm font-serif text-black">
